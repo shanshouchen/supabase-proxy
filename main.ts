1ce8b051e2e1cc9a4017e4f3f2947ddf9d500cb6
shanshouchen <shanshouchen@gmail.com>
2023-08-19 18:57:33 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import {serve} from "https://deno.land/std@0.181.0/http/server.ts";
 import { cors_all } from "https://deno.land/x/masx200_deno_http_middleware@1.0.2/middleware.ts";
 import { createHandler } from "https://deno.land/x/masx200_deno_http_middleware@1.2.1/mod.ts";
 
-const OPENAI_API_HOST = "https://lwksppcglausijzwditv.supabase.co";
+const OPENAI_API_HOST = "lwksppcglausijzwditv.supabase.co";
 
 const handler = createHandler([cors_all, async (ctx) => {
     const url = new URL(ctx.request.url);
